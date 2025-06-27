34e5e149c62466e6e4abf2c8c22cd49fd64b5ee9
boollywoodbot <geetmp3s@gmail.com>
2025-06-27 14:39:35 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -8,6 +8,7 @@ API_HASH = environ.get("API_HASH", "0506d1a8b04f4c580c369b47c885bbd4")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7848383006:AAHt8R3pe4AteA06OF-JMua0ZYYtZF2GsGc")
 OWNER = int(environ.get("OWNER", "7912270773"))
 CREDIT = "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
+LOG_CHANNEL = "-100263153" # log group me bot ko admin narur banaye full rights ke sath
 AUTH_USER = os.environ.get('AUTH_USERS', '7912270773').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
